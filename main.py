d2ee50a097bdfdceae84f72059e7a140364170a6
thunder11bolt <86050508+thunder11bolt@users.noreply.github.com>
2021-06-22 03:12:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# THIS CODE BY THUNDERBOLT
+
+import random
+
+Character = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!$%#@&~`*)(^"
+
+while 1:
+    account_name = input("What is your account : ")
+    password_lenght = int(input("what lenght that you wont your password be : "))
+    password_count = int(input("How many passwords would you like :  "))
+
+    for x in range(0, password_count):
+        ThePassword = ""
+        for x in range(0, password_lenght):
+            password_character = random.choice(Character)
+            ThePassword = ThePassword + password_character
+        print("For This Account : ", account_name, " , Your Password Is: ", ThePassword)
